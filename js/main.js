3159f5210c359e1910d16b08e939fd9cbc6981f9
yjkim <yjkim2200@gmail.com>
2017-01-07 00:11:45 +0900

    added isotope feature(js)

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// init Isotope
+var $container = $('.portfolio-items').isotope('layout');
+
+$('.portfolio-filter').on( 'click', 'a', function(e) {
+	e.preventDefault();
+	var filterValue = $(this).attr('data-filter');
+	$container.isotope({ filter: filterValue });
+
+	$('.portfolio-filter li').removeClass('active');
+	$(this).closest('li').addClass('active');
+});
